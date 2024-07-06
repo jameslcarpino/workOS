2080d3b409c824a77aa15b804e55baa2ec837a48
James Carpino <jamescarpino@drata.com>
2024-07-06 08:55:47 -0700

    mvp of log in directory sync

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import 'dotenv/config'
+import router from './routes/index.js'
+import morgan from 'morgan'
+import { Server } from 'socket.io'
+const app = express()
+
+const port = process.env.PORT || 8000
+
+app.use('/public', express.static('public'))
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.json())
+
+app.use(morgan('dev'))
+
+await app.use('/', router)
+
+// app.listen(port, () => {
+//     console.log(`⚡️ [server]: Server is running at http://localhost:${port}`)
+// })
+
+const server = app.listen(port, () => {
+    console.log(`⚡️ [server]: Server is running at http://localhost:${port}`)
+})
+
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log('connected')
+
+    socket.on('disconnect', () => {
+        console.log('disconnected')
+    })
+})
